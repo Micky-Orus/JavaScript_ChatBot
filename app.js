6bd56c25676eaf28043a3b6650993cefef9cf090
Micky ORUS <49394554+Micky-Orus@users.noreply.github.com>
2019-05-13 09:05:03 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+function QCM() {
+    var réponse = prompt("Comment Ça va?");
+    if (réponse == "Bien") {
+        var réponse = prompt("Quel est votre prénom ?");
+        if (réponse == "Ninja") {
+            var réponse = prompt('Quel âge avez-vous ,');
+            if (réponse == "19 ans") {
+                var réponse = prompt('Êtes-vous Réunionnais ?');
+                if (réponse == "Oui") {
+                    var réponse = prompt('Possédez-vous un ordinateur ?');
+                    if (réponse == "Ouii") {
+                    } else { }
+                } else { }
+            } else { }
+        } else { }
+    } else { }
+}
+alert('Bonjour !')
+
+QCM();
+
+alert('Bienvenue !');
